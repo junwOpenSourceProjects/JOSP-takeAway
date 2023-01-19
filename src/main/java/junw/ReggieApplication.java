f091bea0a97b075359e15c19744b01ae4a6b4a8a
junw <45444154+wo1261931780@users.noreply.github.com>
2023-01-19 13:30:45 +0800

    项目配置完成，使用jdk8

---
+++ src/main/java/junw/ReggieApplication.java
@@ -0,0 +1,26 @@
+package junw;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Intellij IDEA.
+ * Project:reggie_takeaway
+ * Package:junw
+ *
+ * @author liujiajun_junw
+ * @Date 2023-01-13-22  星期四
+ * @description
+ */
+@Slf4j
+@SpringBootApplication
+public class ReggieApplication {
+	// @Slf4j是lombok提供的日志注解
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReggieApplication.class, args);
+		log.debug("spring已经启动了");
+	}
+
+}
