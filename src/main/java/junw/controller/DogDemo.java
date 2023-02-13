f4bb98fd56b5bbc2abd6c1c7644a8ce79a82e332
junw <45444154+wo1261931780@users.noreply.github.com>
2023-02-13 21:21:30 +0800

    dog-chatGPT

---
+++ src/main/java/junw/controller/DogDemo.java
@@ -0,0 +1,18 @@
+package junw.controller;
+
+/**
+ * Created by Intellij IDEA.
+ * Project:reggie_takeaway
+ * Package:junw.controller
+ *
+ * @author liujiajun_junw
+ * @Date 2023-02-18-51  星期一
+ * @description
+ */
+public class DogDemo {
+	// 这是chatGPT给我画的一只狗，如果你看到了狗狗，希望你能开心一点:)
+
+	public static void main(String[] args) {
+		System.out.println("\n        ,\\\n       / \\\n      (   )\n      |___|\n      |___|\n     /|___|\\\n    /_|___|_\\\n    / |___| \\\n   (__|___|__)\n   |_/ \\|_/ \\|\n   / \\_/ \\_/ \\\n   \\     Y     /\n    \\  o| |o  /\n     \\  | |  /\n      \\  \\_/  /\n       \\  |  /\n        \\ | /\n         \\|/\n          V");
+	}
+}
